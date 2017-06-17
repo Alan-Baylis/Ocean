962080d2cff9352ae9273ec0463f414f88b3bd75
kp16 <kp16@outlook.com>
2017-06-17 12:05:05 +0100

    Added editor

---
+++ Editor/LevelEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class LevelEditor : Editor
+{
+    //EditorGUIUtility.ObjectContent(null, typeof(LevelEditor)).image)
+}
+
+[CustomEditor(typeof(Hauberk))]
+public class HauberkEditor : LevelEditor
+{
+	public override void OnInspectorGUI(){
+		base.OnInspectorGUI ();
+		Hauberk h = target as Hauberk;
+		h.GenerateLevel();
+	}
+
+}
+
+[CustomEditor(typeof(Cave))]
+public class CaveEditor : LevelEditor 
+{
+
+	public override void OnInspectorGUI(){
+		base.OnInspectorGUI ();
+		Cave c = target as Cave;
+		c.GenerateLevel();
+	}
+   
+}
